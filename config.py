c9befbe7a807cfda2d6222627493e1dcd558069b
denis-k2 <dxxx999@gmail.com>
2024-05-27 13:21:52 +0700

    add mongodb query

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import getenv
+
+HOST = getenv("HOST")
+PORT = int(getenv("PORT"))
+USERNAME = getenv("USERNAME")
+PASSWORD = getenv("PASSWORD")
+DB = getenv("DB")
+COLLECTION = getenv("COLLECTION")
